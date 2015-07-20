ad93d0343d6a2f8d15a5e0b2b30fa608c64711d0
Daniel Varga <dvarga@skawa.hu>
2015-07-20 14:38:47 +0200

    Adjusted gulp, npm and eslint settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 var gulp = require("gulp"),
+    sourcemaps = require('gulp-sourcemaps'),
     babel = require("gulp-babel"),
     plumber = require("gulp-plumber"),
+    uglify = require("gulp-uglify"),
     es6Path = "es6/*.js",
     compilePath = "es6/compiled";
 
 gulp.task("babel", function () {
     gulp.src([es6Path])
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(babel())
+        .pipe(sourcemaps.write("."))
         .pipe(gulp.dest(compilePath));
 });
 
@@ -17,4 +21,12 @@ gulp.task("watch", function() {
 
 });
 
+gulp.task('compress', function() {
+    return gulp.src(compilePath + "/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
 gulp.task("default", ["babel", "watch"]);
+
+gulp.task("release", ["default", "compress"]);
